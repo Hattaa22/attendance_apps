5f484e5ac8a0ac904eac07f4d200f4a8b74cb7a8
Suhatta <suhatta26@gmail.com>
2025-05-14 14:40:21 +0700

    feat: Setting mneu page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fortis_app/main.dart';
+import 'package:attendance_apps/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
