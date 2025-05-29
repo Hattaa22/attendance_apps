f8ad00778a62fb9ad1969d36d4bdb0770c2ee4d4
Daffa Maulana Satria <daffamaulanasatria@gmail.com>
2025-05-29 23:19:19 +0700

    prep : added dio, secure_storage, provider, dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:fortis_apps/view/splash_screen/splash_screen.dart';
 
 import 'core/router/router.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
